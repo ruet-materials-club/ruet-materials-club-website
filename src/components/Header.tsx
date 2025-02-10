61b6dfa61a2dcda074a3126efbd11589a6996042
Mahmud Nabil <nabilsnigdho@gmail.com>
2025-02-10 15:23:10 +0600

    feat: on scroll transition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Icon = dynamic(() => import("./Icon"), {
   loading: () => <Image src={IconMinified} alt="Icon" className="px-16" />,
@@ -22,101 +22,155 @@ export default function Header({
 }) {
   const pathname = usePathname();
   const [isOpen, setOpen] = useState(false);
-  const isFullPage = pathname === "/";
+  const padRef = useRef<HTMLDivElement>(null);
+  const headerRef = useRef<HTMLElement>(null);
+  const [isFullPage, setIsFullPage] = useState(pathname === "/");
 
   useEffect(() => {
     setOpen(false);
+    if (pathname !== "/") return setIsFullPage(false);
+    const observer1 = new IntersectionObserver(
+      ([e]) => {
+        if (e.intersectionRatio < 0.99 && padRef.current) {
+          padRef.current.style.height = `${window.innerHeight}px`;
+          window.scroll(0, window.innerHeight);
+          setIsFullPage(false);
+        }
+      },
+      { threshold: [0.5, 0.75, 1] },
+    );
+    if (headerRef.current) observer1.observe(headerRef.current);
+    const observer2 = new IntersectionObserver(
+      ([e]) => {
+        if (e.intersectionRatio > 0 && padRef.current) {
+          padRef.current.style.height = "0px";
+          window.scroll(0, 0);
+          setIsFullPage(true);
+        }
+      },
+      { threshold: 0.1 },
+    );
+    if (padRef.current) observer2.observe(padRef.current);
   }, [pathname]);
 
+  const linkClassName = cn(
+    "rounded-2xl border-solid px-4 py-2 text-lg",
+    isFullPage
+      ? "border shadow backdrop-blur-[1px]"
+      : "md:border md:shadow md:backdrop-blur-[1px]",
+  );
+
   return (
-    <header>
-      <div
-        className={cn(
-          "relative flex items-center gap-4 py-4 [@media(max-height:40rem)]:gap-0",
-          isFullPage
-            ? "min-h-svh flex-col justify-center text-center"
-            : "container m-auto px-4",
-        )}
-      >
-        <div className={cn("relative", isFullPage ? "w-full" : "w-auto")}>
-          {isFullPage ? (
-            <>
-              <Icon />
-              <motion.div
+    <>
+      {pathname === "/" && <div ref={padRef}></div>}
+      <header ref={headerRef}>
+        <div
+          className={cn(
+            "relative flex items-center gap-4 py-4 [@media(max-height:40rem)]:gap-0",
+            isFullPage
+              ? "min-h-svh flex-col justify-center text-center"
+              : "container m-auto px-4",
+          )}
+        >
+          <div className={cn("relative", isFullPage ? "w-full" : "w-auto")}>
+            {isFullPage ? (
+              <>
+                <Icon />
+                <motion.div
+                  layout
+                  layoutId="icon"
+                  className="absolute top-0 left-1/2 m-auto aspect-square -translate-x-1/2"
+                  style={{ width: "min(480px, calc(100% - 8rem), 40vh)" }}
+                ></motion.div>
+              </>
+            ) : (
+              <MotionImage
                 layout
                 layoutId="icon"
-                className="absolute top-0 left-1/2 m-auto aspect-square -translate-x-1/2"
-                style={{ width: "min(480px, calc(100% - 8rem), 40vh)" }}
-              ></motion.div>
-            </>
-          ) : (
-            <MotionImage
-              layout
-              layoutId="icon"
-              src={IconMinified}
-              alt="Icon"
-              className="h-12 w-auto"
-            />
-          )}
-        </div>
-        <motion.h1
-          layout
-          className={
-            isFullPage
-              ? "text-3xl font-bold md:text-6xl [@media(max-height:40rem)]:text-xl"
-              : "text-xl"
-          }
-        >
-          RUET Materials Club
-        </motion.h1>
-        {isFullPage && (
-          <motion.div
-            key="motto"
+                src={IconMinified}
+                alt="Icon"
+                className="h-12 w-auto"
+              />
+            )}
+          </div>
+          <motion.h1
+            layout
             className={
               isFullPage
-                ? "text-xl md:text-3xl [@media(max-height:40rem)]:text-lg"
-                : "hidden"
+                ? "text-3xl font-bold md:text-6xl [@media(max-height:40rem)]:text-xl"
+                : "text-xl"
             }
           >
-            Learning. Linking. Leading.
-          </motion.div>
-        )}
-
-        {!isFullPage && (
-          <div className="ms-auto md:hidden">
-            <Hamburger toggled={isOpen} toggle={setOpen} size={24} rounded />
-          </div>
-        )}
-        <motion.nav
-          layout
-          className={cn(
-            "flex gap-4 rounded-3xl",
-            isFullPage ? "absolute bottom-4" : "ms-auto",
-            isFullPage || isOpen || "max-md:hidden",
-            isFullPage ||
-              "top-full right-4 left-4 max-md:absolute max-md:flex-col max-md:border max-md:bg-blue-500/5 max-md:p-2 max-md:backdrop-blur-[2px]",
-          )}
-        >
-          {navigationLinks.map((x) => (
-            <Link
-              key={x.path}
-              href={x.path}
-              className={cn(
-                "rounded-2xl border-solid px-4 py-2 text-lg",
+            RUET Materials Club
+          </motion.h1>
+          {isFullPage && (
+            <motion.div
+              key="motto"
+              className={
                 isFullPage
-                  ? "border shadow backdrop-blur-[1px]"
-                  : "md:border md:shadow md:backdrop-blur-[1px]",
-                pathname === x.path || isFullPage || "max-md:bg-transparent",
-                pathname === x.path
-                  ? "border-b-2 border-b-blue-500/50 bg-blue-500/15"
-                  : "border-blue-500/10 bg-blue-500/5 hover:bg-blue-500/15",
-              )}
+                  ? "text-xl md:text-3xl [@media(max-height:40rem)]:text-lg"
+                  : "hidden"
+              }
             >
-              {x.name}
-            </Link>
-          ))}
-        </motion.nav>
-      </div>
-    </header>
+              Learning. Linking. Leading.
+            </motion.div>
+          )}
+
+          {!isFullPage && (
+            <div className="ms-auto md:hidden">
+              <Hamburger toggled={isOpen} toggle={setOpen} size={24} rounded />
+            </div>
+          )}
+          <motion.nav
+            layout
+            className={cn(
+              "flex gap-4 rounded-3xl",
+              isFullPage ? "absolute bottom-4" : "ms-auto",
+              isFullPage || isOpen || "max-md:hidden",
+              isFullPage ||
+                "top-full right-4 left-4 max-md:absolute max-md:flex-col max-md:border max-md:bg-blue-500/5 max-md:p-2 max-md:backdrop-blur-[2px]",
+            )}
+          >
+            {navigationLinks.map((x) =>
+              pathname === "/" && x.path === "/" ? (
+                <button
+                  className={cn(
+                    linkClassName,
+                    "cursor-pointer border-b-2 border-b-blue-500/50 bg-blue-500/15",
+                  )}
+                  key={x.path}
+                  onClick={() => window.scroll(0, window.innerHeight)}
+                >
+                  {x.name}
+                </button>
+              ) : (
+                <Link
+                  key={x.path}
+                  href={x.path}
+                  className={cn(
+                    linkClassName,
+                    pathname === x.path ||
+                      isFullPage ||
+                      "max-md:bg-transparent",
+                    pathname === x.path
+                      ? "border-b-2 border-b-blue-500/50 bg-blue-500/15"
+                      : "border-blue-500/10 bg-blue-500/5 hover:bg-blue-500/15",
+                  )}
+                >
+                  {x.name}
+                </Link>
+              ),
+            )}
+          </motion.nav>
+        </div>
+      </header>
+      {pathname === "/" && (
+        <style>{`
+          body {
+            min-height: 200vh;
+          }
+        `}</style>
+      )}
+    </>
   );
 }
