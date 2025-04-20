63343ed6325bc9877937e04e2c4b0a96c7f99aa2
Mahmud Nabil <nabilsnigdho@gmail.com>
2025-04-20 18:50:04 +0600

    feat: payload

---
+++ src/payload.config.ts
@@ -0,0 +1,23 @@
+import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+
+export default buildConfig({
+  // If you'd like to use Rich Text, pass your editor here
+  editor: lexicalEditor(),
+
+  // Define and configure your collections in this array
+  collections: [],
+
+  // Your Payload secret - should be a complex and secure string, unguessable
+  secret: process.env.PAYLOAD_SECRET || "",
+  // Whichever Database Adapter you're using should go here
+  // Mongoose is shown as an example, but you can also use Postgres
+  db: vercelPostgresAdapter(),
+  // If you want to resize images, crop, set focal point, etc.
+  // make sure to install it and pass it to the config.
+  // This is optional - if you don't need to do these things,
+  // you don't need it!
+  sharp,
+});
